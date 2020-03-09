9294ade162c18466c2f687f8e7e85c5b73024865
James Dang <jamesfromeastlondon@gmail.com>
2020-03-09 20:11:23 +0100

    created productsController with parameters

---
+++ WebApi/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Product(long id)
+        {
+            return View();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Product(long id, string description)
+        {
+            return View();
+        }
+    }
+}

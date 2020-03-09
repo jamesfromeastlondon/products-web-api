c09953844999bda43eec126a0388e0e02a1559f2
James Dang <jamesfromeastlondon@gmail.com>
2020-03-09 20:06:13 +0100

    created product model, installed nuget packages

---
+++ WebApi/WebApi/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ImgUri { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public Product(string name, string imgUri, decimal price, string description)
+        {
+            Name = name;
+            ImgUri = imgUri;
+            Price = price;
+            Description = description;
+        }
+    }
+}

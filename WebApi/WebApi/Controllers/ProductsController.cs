402655baa152d2425429cb19158423a133489d5f
James Dang <jamesfromeastlondon@gmail.com>
2020-03-09 20:35:51 +0100

    added functionality to productsController

--- WebApi/WebApi/Controllers/ProductsController.cs
+++ WebApi/WebApi/Controllers/ProductsController.cs
@@ -1,26 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+        private readonly ProductService productService;
+        public ProductsController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
         [HttpGet]
         public IActionResult Products()
         {
-            return View();
+            List<Product> products = productService.GetAll();
+            return Json(new { products });
         }
 
         [HttpGet("{id}")]
         public IActionResult Product(long id)
         {
-            return View();
+            Product product = productService.GetById(id);
+            if (product != null)
+                return Json(new { product });
+            return NotFound();
         }
 
         [HttpPatch("{id}")]
         public IActionResult Product(long id, string description)
         {
-            return View();
+            bool isUpdated = productService.UpdateProduct(id, description);
+            if (isUpdated)
+                return NoContent();
+            return NotFound();
         }
     }
 }
